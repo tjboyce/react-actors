038c782bca6e7c081e02a2845194c308b99eb31c
Todd Boyce Jr <tjboyce@me.com>
2019-02-19 18:03:47 -0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+//  import './components/App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            
+                <header>
+                <h1>React Actors</h1>
+                </header>
+        
+        );
+    }
+}
+
+export default Header;
